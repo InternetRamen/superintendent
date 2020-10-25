9d7309536febdd28252b33cb4d12a76b96345802
unknown <stevez9101@gmail.com>
2020-10-25 15:54:57 -0400

    fixed perms

---
+++ src/cmds/code-bounties/completeb.ts
@@ -0,0 +1,12 @@
+import { Client, Message, MessageEmbed } from "discord.js-light";
+
+module.exports.run = async (client: Client, message: Message, args: string[]) => {
+
+}
+module.exports.help = {
+	name: "completeb",
+	description: "A command that allows operators to mark a bounty as complete.",
+	aliases: [],
+	usage: "?eval <code>",
+	permissions: "<@&766011830355099738>",
+}
